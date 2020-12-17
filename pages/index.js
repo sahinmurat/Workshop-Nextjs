420c4e5286dffcb9aae034d74f3fce64cdc3521f
Murat Sahin <yazilimogren1111@gmail.com>
2020-12-17 20:56:10 +0100

    layout created

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import SvgComponent from '../components/Add'
+import {TimelineProp} from '../components/icons'
 
 export default function Home() {
   return (
+    <div className= {styles.container}>
     <SvgComponent />
+    <TimelineProp/>
+    </div>
   )   
  }
    
+ 
